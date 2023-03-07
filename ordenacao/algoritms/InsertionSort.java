0089c08e4c4c0bcd54c00b4b0bbe77ec44147001
Matth-P <109482342+Matth-P@users.noreply.github.com>
2023-03-07 20:52:52 +0000

    Start

--- ordenacao/algoritms/InsertionSort.java
+++ ordenacao/algoritms/InsertionSort.java
@@ -7,13 +7,31 @@ public class InsertionSort implements Sorter{
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+        for (int i = 1; i < sorted.length; i++){
+            int valor = sorted[i];
+            int j = i - 1;
+            while(j >= 0 && sorted[j] > valor){
+                sorted[j + 1] = sorted[j];
+                j = j - 1;
+            }
+            sorted[j + 1] = valor;
+        }
         return sorted;
     }
 
     @Override
     public String getName() {
         return "InsertionSort";
+    }
+
+    public static void main(String[] args) {
+        InsertionSort i = new InsertionSort();
+        int[] arrayOr = {3,2,1,5};
+        int[] arrayOrd = i.sort(arrayOr);
+        for (int j : arrayOrd) {
+            System.out.println(j);
+        }
+        
     }
 
 }
--- ordenacao/algoritms/MergeSort.java
+++ ordenacao/algoritms/MergeSort.java
@@ -7,7 +7,18 @@ public class MergeSort implements Sorter {
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+        //int num = 1;
+        //int start, mid, end;
+        //while (num < sorted.length){
+        //    start = 0;
+        //    while (start + num < sorted.length){
+        //        mid = start + num;
+        //        end = start + 2 * num;
+        //        if (end > sorted.length){
+        //            end = sorted.length
+        //        }
+        //    }
+        //}
         return sorted;
     }
 
